5508facabcc0aeb7f3f3c8fda051e4f19ef275a7
Deepak Kumar <deepakk87@gmail.com>
2023-03-06 15:47:21 -0800

    Basic react router functionality using outlet/layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,73 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  redirect
+} from "react-router-dom";
 import './App.css';
+
+function Home() {
+  return <>
+    This is the home Page
+  </>
+}
+
+function About() {
+  return <>
+    This is the home Page
+  </>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    // We can use the children data to create a menu/nav bar
+    children: [
+        {
+          index:true,
+          loader: async ()=> {
+            // Programatically redirect to right page.
+            return redirect('/home');
+          }
+        },
+        {
+          path: "home",
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+    ]
+  },
+]);
+
+
+
+function Layout(props: any) {
+  return <div>
+    <div> Hello to our site </div>
+    {props.children}
+    <div>©2023, 14768540 Canada, Inc.</div>
+  </div>
+}
+
+
+function Root() {
+  return <Layout>
+    <Outlet/>
+  </Layout>
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-          Learn React
-    </div>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
